24d5dcf0c87fde4e92c2f91089ca3aefda432fab
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-24 17:35:07 -0300

    Dev: Initialize project and create a basic navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import type {NextPage} from "next";
 import Image from "next/image";
 
-import logo from "../public/logo.svg";
-
 const Home: NextPage = () => {
   return (
-    <div className="h-full flex bg-black">
-      <header className="m-auto text-white text-center">
-        <Image alt="Basement" src={logo} />
-        <h4>Lets get this party started</h4>
-      </header>
+    <div className="xl w-full">
+      <div className="flex flex-row justify-between w-full px-5 py-4">
+        <Image alt="Basement's logo" height={28} src="/logo.svg" width={192} />
+        <div className="flex flex-row gap-x-4">
+          <Image alt="hola" height={22} src="/icons/navbar-icon-5.svg" width={22} />
+          <Image alt="hola" height={24} src="/icons/navbar-icon-2.svg" width={42} />
+          <Image alt="hola" height={20} src="/icons/navbar-icon-4.svg" width={82} />
+          <Image alt="hola" height={24} src="/icons/navbar-icon-3.svg" width={42} />
+          <Image alt="hola" height={21} src="/icons/navbar-icon.svg" width={24} />
+          {/* <Image />
+          <Image />
+          <Image />
+          <Image /> */}
+        </div>
+        <button className="font-bold text-lg rounded-3xl border-white border-2 px-4 py-2">
+          CART (0)
+        </button>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      grotesque: ["Basement Grotesque"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
