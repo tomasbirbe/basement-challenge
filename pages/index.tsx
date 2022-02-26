ef69906886eb9dfc9f47fe3f05a7291530d05950
Tomas Birbe <tomas.birbe@gmail.com>
2022-02-26 11:28:41 -0300

    Dev: Rename files of icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,17 +7,13 @@ const Home: NextPage = () => {
       <div className="flex flex-row justify-between w-full px-5 py-4">
         <Image alt="Basement's logo" height={28} src="/logo.svg" width={192} />
         <div className="flex flex-row gap-x-4">
-          <Image alt="hola" height={22} src="/icons/navbar-icon-5.svg" width={22} />
+          <Image alt="hola" height={22} src="/icons/navbar-icon-1.svg" width={22} />
           <Image alt="hola" height={24} src="/icons/navbar-icon-2.svg" width={42} />
-          <Image alt="hola" height={20} src="/icons/navbar-icon-4.svg" width={82} />
-          <Image alt="hola" height={24} src="/icons/navbar-icon-3.svg" width={42} />
-          <Image alt="hola" height={21} src="/icons/navbar-icon.svg" width={24} />
-          {/* <Image />
-          <Image />
-          <Image />
-          <Image /> */}
+          <Image alt="hola" height={20} src="/icons/navbar-icon-3.svg" width={82} />
+          <Image alt="hola" height={24} src="/icons/navbar-icon-4.svg" width={42} />
+          <Image alt="hola" height={21} src="/icons/navbar-icon-5.svg" width={24} />
         </div>
-        <button className="font-bold text-lg rounded-3xl border-white border-2 px-4 py-2">
+        <button className="font-bold font-grotesque text-lg rounded-3xl border-white border-2 px-4 py-2">
           CART (0)
         </button>
       </div>
